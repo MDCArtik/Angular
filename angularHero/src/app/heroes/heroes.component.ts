d85f43cbcc7acaff6a6d82a21f151588c7d9c14e
Decker <matthieu.decker@artik-consulting.com>
2021-02-07 13:13:05 +0100

    display list

--- angularHero/src/app/heroes/heroes.component.ts
+++ angularHero/src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../app.hero';
 import { HEROES } from '../app.mock-heroes';
 
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,8 +13,13 @@ import { HEROES } from '../app.mock-heroes';
 export class HeroesComponent implements OnInit {
   
   heroes = HEROES;
+  selectedHero: Hero = HEROES[0];
+  
   constructor() { }
 
   ngOnInit(): void {
+}
+ onSelect(hero: Hero): void {
+  this.selectedHero = hero;
 }
 }
